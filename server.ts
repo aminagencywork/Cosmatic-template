ea50d60655e371a7311c79a4412ec3aa6cf3c2a9
mdAmmu <mdaminpopatiya123@gmail.com>
2026-04-02 01:51:59 +0530

    fourth commit

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import * as cookie from "cookie";
 import * as jwt from "jsonwebtoken";
-import { config } from "./src/config.ts";
+import { config } from "./src/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
